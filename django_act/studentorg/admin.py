318870b9a9d78cf5850fa224289e69f968203287
Don202180208 <202180208@psu.palawan.edu.ph>
2024-04-02 09:00:21 +0800

    models and admin working properly

--- django_act/studentorg/admin.py
+++ django_act/studentorg/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import College, Program, Organization, Student, OrgMember
+
+admin.site.register(College)
+admin.site.register(Program)
+admin.site.register(Organization)
+admin.site.register(Student)
+admin.site.register(OrgMember)
 
 # Register your models here.
--- django_act/studentorg/models.py
+++ django_act/studentorg/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class College (BaseModel):
+    college_name = models.CharField(max_length=150)
+
+
+    def str_(self):
+        return self.college_name
+
+class Program (BaseModel): 
+    prog_name = models.CharField(max_length=150) 
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+
+    def _str (self): 
+        return self.prog_name
+
+class Organization (BaseModel):
+    name = models. CharField(max_length=250) 
+    college = models.ForeignKey(
+        College, null=True, blank=True, on_delete=models.CASCADE)
+
+    description = models.CharField(max_length=500)
+
+    def str (self): 
+        return self.name
+
+class Student(BaseModel):
+    student_id = models.CharField(max_length=15) 
+    lastname = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=25)
+    middlename = models.CharField(max_length=25, blank=True, null=True)
+    program = models. ForeignKey (Program, on_delete=models.CASCADE)
+
+    def str (self):
+
+        return f"(self.lastname), (self.firstname)"
+
+class OrgMember(BaseModel):
+    student = models. ForeignKey(Student, on_delete=models.CASCADE)
+    organization = models. ForeignKey(Organization, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+
 # Create your models here.
