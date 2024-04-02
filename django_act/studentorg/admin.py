fda877501943c8dad89856f2554dc1df0b5597ab
Don202180208 <202180208@psu.palawan.edu.ph>
2024-04-02 09:53:35 +0800

    refactor admin.py

--- django_act/studentorg/admin.py
+++ django_act/studentorg/admin.py
@@ -4,7 +4,32 @@ from .models import College, Program, Organization, Student, OrgMember
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
+
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("get_lastname", "get_firstname", "get_middlename", "get_program")
+    search_fields = ("student__lastname", "student__firstname",)
+
+    def get_lastname(self, obj):
+        return obj.student.lastname
+
+    def get_firstname(self, obj):
+        return obj.student.firstname
+
+    def get_middlename(self, obj):
+        return obj.student.middlename
+
+    def get_program(self, obj):
+        return obj.student.program
+
+    get_lastname.short_description = "Last Name"
+    get_firstname.short_description = "First Name"
+    get_middlename.short_description = "Middle Name"
+    get_program.short_description = "Program"
 
 # Register your models here.
