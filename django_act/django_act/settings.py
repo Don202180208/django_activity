429582f4089d381228d4766cdd581c6a00c7b352
Don202180208 <202180208@psu.palawan.edu.ph>
2024-04-08 00:30:35 +0800

    Changed ALLOWED_HOSTS

--- django_act/django_act/settings.py
+++ django_act/django_act/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1rrs2m0g#ncp7wofbfj)wksf9-m8&3csgmw&8!!z%0eb=cvom7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['ITMS.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
